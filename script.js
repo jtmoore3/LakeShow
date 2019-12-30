5c76ee478530f0f07f7d0f4f41020e2c6dfe1722
Jalen Moore <jtmoore3@aggies.ncat.edu>
2019-12-30 10:16:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeImage(text) {
+    var change = document.getElementById('down_arrow');
+    change.innerHTML = text;
+}
+
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
